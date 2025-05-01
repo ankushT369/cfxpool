f1b182f79bdba46f2d541abd48dab9aefef25984
Ankush Mondal <mondalankush9851@gmail.com>
2025-05-01 16:52:07 +0530

    small fix

--- example/main.c
+++ example/main.c
@@ -9,6 +9,7 @@ void bug() {
 int main() {
         fx_pool pool;
         fx_error err = fxpool_create(4, B, 883, SYS_DEF, &pool, (FXPOOL_SIZE_AUTO_GROW | FXPOOL_SIZE_CUSTOM));
+        fxpool_log(&pool);
 
         int* ptr;
         ptr = (int*)fxpool_alloc(&pool);
